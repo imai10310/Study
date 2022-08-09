583279ceed2f02a2242cf5c803a3797f77599831
“imai10310X” <17039vbu@gmail.com>
2022-08-09 22:59:14 +0900

    初期ファイル設置

--- page-list.js
+++ page-list.js
@@ -1,5 +1,5 @@
 /** レッスンディレクトリ */
-const lessons = ['Lesson1', 'Lesson2', 'Lesson3', 'Lesson4'];
+const lessons = ['Lesson1', 'Lesson2', 'Lesson3', 'Lesson4', 'Lesson5'];
 
 /** 教えてもらったディレクトリ */
 const teaches = ['position'];
